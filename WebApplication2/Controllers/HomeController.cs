5e2090923d962fbebf8fcc4b5d87b70a3a267742
Danny Wong <wong.danny29@gmail.com>
2019-04-19 00:06:55 -0700

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formFields)
+
+        {
+
+            string Fieldvalue1 = formFields.Get("Description");
+            char[] charArray = Fieldvalue1.ToCharArray();
+            Array.Reverse(charArray);
+
+
+            var reversedWords = string.Join(" ", Fieldvalue1.Split(' ').Select(x => new String(x.Reverse().ToArray())).ToArray());
+
+            ViewData["reversedString"] = new string(charArray);
+            ViewData["reversedWords"] = reversedWords;
+            return View();
+
+        }
+
+
+    }
+}
